5abdb14e616198f1b857ca444b3cc66e4b78d875
FarahDvp <farahdridy@gmail.com>
2025-03-26 11:18:39 +0100

    db config

--- backend_node/configs/mongoConfig.js
+++ backend_node/configs/mongoConfig.js
@@ -6,6 +6,6 @@ module.exports.ConnectMongo = () => {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         })
-        .then(() => console.log("MongoDB is connected "))
+        .then(() => console.log("MongoDB is connected !"))
         .catch((err) => console.log(err));
 };
