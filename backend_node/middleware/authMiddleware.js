380a83980344aa4badf6c1f9169f1b2297885e13
FarahDvp <farahdridy@gmail.com>
2025-03-26 13:11:22 +0100

    add auth middleware

---
+++ backend_node/middleware/authMiddleware.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+const protect = async (req, res, next) => {
+    let token;
+
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+        try {
+            token = req.headers.authorization.split(' ')[1];
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+            req.user = await User.findById(decoded.id).select('-password');
+            next();
+        } catch (error) {
+            console.error('Erreur de vérification du token:', error);
+            res.status(401).json({
+                message: 'Accès non autorisé, token invalide',
+                error: error.message
+            });
+        }
+    } else {
+        res.status(401).json({ message: 'Accès non autorisé, aucun token fourni' });
+    }
+};
+
+module.exports = protect;
